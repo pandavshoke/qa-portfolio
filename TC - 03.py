3a0be1fc25ac76010f93980afe97f47293d4afc5
pandavshoke <vshokepanda@gmail.com>
2026-06-10 11:54:19 +0300

    add TC-02 test

--- TC - 03.py
+++ TC - 03.py
@@ -6,12 +6,12 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
 driver.get("https://www.saucedemo.com")
-wait= WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 10)
 wait.until(EC.presence_of_element_located((By.ID, "user-name")))
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
-driver.find_element(By.ID, "password").send_keys("123")
+driver.find_element(By.ID, "user-name").send_keys("")
+driver.find_element(By.ID, "password").send_keys("")
 driver.find_element(By.ID, "login-button").click()
 error = driver.find_element(By.CSS_SELECTOR, "[data-test='error']")
-assert "Epic sadface: Username and password do not match any user in this service" in error.text
+assert "Epic sadface: Username is required" in  error.text
 print("Форма заполнена!")
 input("Нажми Enter чтобы закрыть...")
