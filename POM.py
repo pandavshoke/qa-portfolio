8075c02b51f2fbdbdaec163c45cb973fed814f31
pandavshoke <vshokepanda@gmail.com>
2026-06-19 12:56:56 +0300

    pom 2.0

--- POM.py
+++ POM.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 
 class LoginPage:
@@ -21,3 +22,19 @@ class InventoryPage:
        items = self.driver.find_elements(By.CSS_SELECTOR, "[data-test='inventory-item-name']")
        first_item = items[0]
        return first_item.text
+    def sort_by(self, option):
+        dropdown = Select(self.driver.find_element(By.CLASS_NAME, "product_sort_container"))
+        dropdown.select_by_visible_text(option)
+    def add_to_cart(self, item_name):
+        self.driver.find_element(By.CSS_SELECTOR, f"[data-test='add-to-cart-{item_name}']").click()
+    def go_to_cart(self):
+        self.driver.find_element(By.CSS_SELECTOR, "[data-test='shopping-cart-link']").click()
+class CartPage:
+    def __init__(self, driver):
+        self.driver = driver
+    def get_cart_items(self):
+        cart_items = self.driver.find_elements(By.CSS_SELECTOR, "[data-test='inventory-item-name']")
+        return [item.text for item in cart_items]
+    def click_checkout(self):
+        self.driver.find_element(By.ID, "checkout").click()
+    
--- test_POM.py
+++ test_POM.py
@@ -1,5 +1,7 @@
-from POM import LoginPage, InventoryPage
+from POM import LoginPage, InventoryPage, CartPage
+from selenium.webdriver.common.by import By
 import pytest
+from selenium.webdriver.support.ui import Select
 
 def test_login(driver):
     page = LoginPage(driver)
@@ -7,3 +9,24 @@ def test_login(driver):
     page.enter_password("secret_sauce")
     page.click_login()
     assert "Swag Labs" in driver.title
+def test_sorting(driver):
+    page = LoginPage(driver)
+    page.enter_username("standard_user")
+    page.enter_password("secret_sauce")
+    page.click_login()
+    page2 = InventoryPage(driver)
+    page2.sort_by("Name (Z to A)")
+    result = page2.get_first_item_name()
+    assert "Test.allTheThings() T-Shirt (Red)" in result
+def test_cart_page(driver):
+    page = LoginPage(driver)
+    page.enter_username("standard_user")
+    page.enter_password("secret_sauce")
+    page.click_login()
+    page2 = InventoryPage(driver)
+    page2.add_to_cart("sauce-labs-bolt-t-shirt")
+    page2.go_to_cart()
+    page3 = CartPage(driver)
+    result = page3.get_cart_items()
+    assert "Sauce Labs Bolt T-Shirt" in result
+
