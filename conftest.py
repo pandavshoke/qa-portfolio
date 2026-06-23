5ec0d0508e2b54dc31a55b90b79cd3434c308ab9
pandavshoke <vshokepanda@gmail.com>
2026-06-23 12:20:47 +0300

    add CI

--- conftest.py
+++ conftest.py
@@ -2,14 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 @pytest.fixture
 def driver():
     options = Options()
     options.add_argument("--headless")
-    driver = webdriver.Firefox(options=options)
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.saucedemo.com")
     yield driver
     driver.quit()
